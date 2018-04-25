d8340d3724db638b43f6769c3b05201ff1438764
leochirag <crao3@hawk.iit.edu>
2018-04-25 17:29:42 -0500

    table cleanup

--- coursegpa.php
+++ coursegpa.php
@@ -174,9 +174,9 @@ $(function(){
 							<li class="current"><a href="index.php">ACADEMICS<span class="arrow"></span></a>
 								<ul style="display: none;" class="sub_menu">
 									<li class="arrow_top"></li>
-									<li><a class="subCurrent" href="coursegpa.php">Current GPA</a></li>
+									<li><a class="subCurrent" href="coursegpa.php">Current Grades</a></li>
 									<li><a href="calc.php">GPA Calculator</a></li>
-									<li><a href="login.php">Class Schedule</a></li>
+									<li><a href="coursescheduler.php">Class Schedule</a></li>
 									<li><a href="courselist.php">Courses List</a></li>
 									<li><a href="https://web.iit.edu/registrar/academic-calendar">Academic Calender</a></li>
 								</ul>
@@ -202,7 +202,7 @@ $(function(){
 									<li class="arrow_top"></li>
 									<li><a class="subCurrent" href="https://web.iit.edu/sites/web/files/departments/about-iit/pdfs/campus-map.pdf">University map</a></li>
 									<li><a href="https://web.iit.edu/contact-us">Important contact Info</a></li>
-									<li><a href="https://hawklink.iit.edu/Organizations">Weather Updates</a></li>
+									<li><a href="https://weather.com/weather/today/l/USIL0225:1:US">Weather Updates</a></li>
 									<li><a href="https://web.iit.edu/sites/web/files/departments/acaps/pdfs/Illinois-Tech-Shuttle-Bus-Schedule.pdf">Shuttle Bus</a></li>
 								</ul>
 							</li>
@@ -215,9 +215,9 @@ $(function(){
 		<caption class="title">GPA</caption>
 		<thead>
 			<tr>
-				<th>COURSE</th>
+				<th>COURSENO</th>
 				<th>SEMESTER</th>
-				<th>GPA</th>
+				<th>GRADE</th>
 
 			</tr>
 		</thead>
@@ -230,9 +230,9 @@ $(function(){
 
 			echo '<tr>
 
-					<td>'.$row['Course'].'</td>
+					<td>'.$row['CourseNo'].'</td>
 					<td>'.$row['Semester'].'</td>
-					<td>'.$row['GPA'].'</td>
+					<td>'.$row['Grade'].'</td>
 					</tr>';
 
 			$no++;
